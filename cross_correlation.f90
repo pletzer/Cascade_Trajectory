facfae673094d6c9fb5573cedd6f426271ecba56
Alex Pletzer <alexander@gokliya.net>
2018-11-22 15:36:18 +1300

    ~ 1:3 -> : (code simplification)

--- cross_correlation.f90
+++ cross_correlation.f90
@@ -433,7 +433,7 @@ PROGRAM Two_Filter_Cross_Correlation
           cassig_b = -i * SQRT(0.5 * gamma * kappa_b) * SQRT(1.0 * nb) * sigmam
           ! Matrix multiplication
           DO m=1,3
-            k1(nplace + m) = dot_product(H_full(m,1:3), &
+            k1(nplace + m) = dot_product(H_full(m,:), &
                            &  psi_clone(nplace + 1:nplace + 3))
             ! Only calculate the next part for photon number na > 0 as the
             ! a^{\dagger} operator couples the |N-1> state to the |N> state.
@@ -443,14 +443,14 @@ PROGRAM Two_Filter_Cross_Correlation
               ! cavity a
               ! photon number index for (na - 1)
               nplace_pm1 = (3 * (N + 1) * (na - 1)) + (3 * nb)
-              temp1 = dot_product(cassig_a(m,1:3), &
+              temp1 = dot_product(cassig_a(m,:), &
                     &  psi_clone(nplace_pm1 + 1:nplace_pm1 + 3))
             END IF
             IF (nb /= 0) THEN
                ! cavity b
                ! photon number index for (nb - 1)
                nplace_pm1 = (3 * (N + 1) * na) + (3 * (nb - 1))
-               temp2 = dot_product(cassig_b(m,1:3), &
+               temp2 = dot_product(cassig_b(m,:), &
                      &  psi_clone(nplace_pm1 + 1:nplace_pm1 + 3))
             END IF
             ! Update k vector
@@ -483,7 +483,7 @@ PROGRAM Two_Filter_Cross_Correlation
           cassig_b = -i * SQRT(0.5 * gamma * kappa_b) * SQRT(1.0 * nb) * sigmam
           ! Matrix multiplication
           DO m=1,3
-            k2(nplace + m) = dot_product(H_full(m,1:3), &
+            k2(nplace + m) = dot_product(H_full(m,:), &
                            &  psi_clone(nplace + 1:nplace + 3) + &
                            &    0.5 * k1(nplace + 1:nplace + 3))
             ! Only calculate the next part for photon number l > 0 as the
@@ -493,7 +493,7 @@ PROGRAM Two_Filter_Cross_Correlation
               ! cavity a
               ! photon number index for (na - 1)
               nplace_pm1 = (3 * (N + 1) * (na - 1)) + (3 * nb)
-              temp1 = dot_product(cassig_a(m,1:3), &
+              temp1 = dot_product(cassig_a(m,:), &
                     &  psi_clone(nplace_pm1 + 1:nplace_pm1 + 3) + &
                     &    0.5 * k1(nplace_pm1 + 1:nplace_pm1 + 3))
              END IF
@@ -502,7 +502,7 @@ PROGRAM Two_Filter_Cross_Correlation
                 ! cavity b
                 ! photon number index for (nb - 1)
                 nplace_pm1 = (3 * (N + 1) * na) + (3 * (nb - 1))
-                temp2 = dot_product(cassig_b(m,1:3), &
+                temp2 = dot_product(cassig_b(m,:), &
                       &  psi_clone(nplace_pm1 + 1:nplace_pm1 + 3) + &
                       &    0.5 * k1(nplace_pm1 + 1:nplace_pm1 + 3))
               END IF
@@ -536,7 +536,7 @@ PROGRAM Two_Filter_Cross_Correlation
           cassig_b = -i * SQRT(0.5 * gamma * kappa_b) * SQRT(1.0 * nb) * sigmam
           ! Matrix multiplication
           DO m=1,3
-            k3(nplace + m) = dot_product(H_full(m,1:3), &
+            k3(nplace + m) = dot_product(H_full(m,:), &
                            &  psi_clone(nplace + 1:nplace + 3) + &
                            &    0.5 * k2(nplace + 1:nplace + 3))
            ! Only calculate the next part for photon number l > 0 as the
@@ -546,7 +546,7 @@ PROGRAM Two_Filter_Cross_Correlation
               ! cavity a
               ! photon number index for (na - 1)
               nplace_pm1 = (3 * (N + 1) * (na - 1)) + (3 * nb)
-              temp1 = dot_product(cassig_a(m,1:3), &
+              temp1 = dot_product(cassig_a(m,:), &
                     & psi_clone(nplace_pm1 + 1:nplace_pm1 + 3) + &
                     &   0.5 * k2(nplace_pm1 + 1:nplace_pm1 + 3))
             END IF
@@ -555,7 +555,7 @@ PROGRAM Two_Filter_Cross_Correlation
                ! cavity b
                ! photon number index for (nb - 1)
                nplace_pm1 = (3 * (N + 1) * na) + (3 * (nb - 1))
-               temp2 = dot_product(cassig_b(m,1:3), &
+               temp2 = dot_product(cassig_b(m,:), &
                      & psi_clone(nplace_pm1 + 1:nplace_pm1 + 3) + &
                      &   0.5 * k2(nplace_pm1 + 1:nplace_pm1 + 3))
              END IF
@@ -589,7 +589,7 @@ PROGRAM Two_Filter_Cross_Correlation
           cassig_b = -i * SQRT(0.5 * gamma * kappa_b) * SQRT(1.0 * nb) * sigmam
           ! Matrix multiplication
           DO m=1,3
-            k4(nplace + m) = dot_product(H_full(m,1:3), &
+            k4(nplace + m) = dot_product(H_full(m,:), &
                            & psi_clone(nplace + 1:nplace + 3) + &
                                k3(nplace + 1:nplace + 3))
            ! Only calculate the next part for photon number l > 0 as the
@@ -599,7 +599,7 @@ PROGRAM Two_Filter_Cross_Correlation
               ! cavity a
               ! photon number index for (na - 1)
               nplace_pm1 = (3 * (N + 1) * (na - 1)) + (3 * nb)
-              temp1 = dot_product(cassig_a(m,1:3), &
+              temp1 = dot_product(cassig_a(m,:), &
                     & psi_clone(nplace_pm1 + 1:nplace_pm1 + 3) + &
                     & k3(nplace_pm1 + 1:nplace_pm1 + 3))
             END IF
@@ -608,7 +608,7 @@ PROGRAM Two_Filter_Cross_Correlation
                ! cavity b
                ! photon number index for (nb - 1)
                nplace_pm1 = (3 * (N + 1) * na) + (3 * (nb - 1))
-               temp2 = dot_product(cassig_b(m,1:3), &
+               temp2 = dot_product(cassig_b(m,:), &
                      & psi_clone(nplace_pm1 + 1:nplace_pm1 + 3) + &
                      &   k3(nplace_pm1 + 1:nplace_pm1 + 3))
              END IF
